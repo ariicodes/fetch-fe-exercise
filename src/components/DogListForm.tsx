6e5748f682621818f449d8d3e5467b9e8294f6dd
Ariela Israel <arielaisrael12@gmail.com>
2024-06-12 13:20:09 -0400

    +DogListForm modified to call api for images

--- src/components/DogListForm.tsx
+++ src/components/DogListForm.tsx
@@ -1,13 +1,63 @@
-const DogListForm = ({ dogList }: { dogList: string[] }) => {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface DogListFormProps {
+	dogList: string[];
+	setImages: (newImages: string[]) => void;
+}
+
+const DogListForm: React.FC<DogListFormProps> = ({ dogList, setImages }) => {
+	const [options, setOptions] = useState<string[]>([]);
+
 	// FORMATTING BREED VALUES
 	const formatBreed = (breed: string) => {
-		return breed.split(' ').join('-');
+		const breedSplit = breed.split(' ');
+		return breedSplit.join('-');
 	};
 
+	const getImages = async (source: string) => {
+		try {
+			const res = await axios.get(source);
+			const data = await res.data.message;
+			// Return the fetched image URLs
+			return data;
+		} catch (err) {
+			console.error('ERROR:', err);
+			// Handle errors appropriately (optional)
+			return []; // Return empty array to avoid issues
+		}
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		const selectedOpts = Array.from(e.target.selectedOptions).map(
+			option => `https://dog.ceo/api/breed/${option.value}/images`
+		);
+		setOptions(selectedOpts);
+	};
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault(); // Prevent default form submission
+
+		const allImageUrls: string[] = [];
+		// Fetch images for each selected option
+		for (const option of options) {
+			const fetchedImages = await getImages(option);
+			allImageUrls.push(...fetchedImages);
+		}
+
+		// Set the final images state after all fetches are complete
+		setImages(allImageUrls);
+	};
+
+	useEffect(() => {
+		// Fetch images for initially selected options
+		options.forEach(getImages);
+	}, [options]);
+
 	return (
-		<form className='px-24 flex flex-col'>
+		<form className='flex flex-col mb-16' onSubmit={handleSubmit}>
 			<div className='flex flex-col mb-4'>
-				<label htmlFor='breed-select' className='font-bold'>
+				<label htmlFor='breed-select' className='font-bold mb-1 pl-1 text-xl'>
 					Select one or more breeds{' '}
 					<span className='font-extralight'>
 						Hold ctrl/cmd while clicking to select multiple
@@ -17,7 +67,8 @@ const DogListForm = ({ dogList }: { dogList: string[] }) => {
 					name='breed-select'
 					id='breed-select'
 					multiple
-					className='border-2 rounded-lg p-2'
+					className='border-2 rounded-lg px-10 py-2 bg-slate-700 text-lg h-52'
+					onChange={handleChange}
 				>
 					{dogList &&
 						dogList.map((dog, i) => (
