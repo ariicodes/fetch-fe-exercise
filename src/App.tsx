7916480ef75bd0c962512f59e4270c1280fdf7a8
Ariela Israel <arielaisrael12@gmail.com>
2024-06-12 20:36:16 -0400

    +modified back to top btn

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Hero from './components/Hero';
 function App() {
 	const [dogList, setDogList] = useState<string[]>([]);
 	const [images, setImages] = useState<string[]>([]);
+	const [showBtn, setShowBtn] = useState<boolean>(false);
 
 	useEffect(() => {
 		const getDogs = async () => {
@@ -35,12 +36,30 @@ function App() {
 		getDogs();
 	}, []);
 
+	useEffect(() => {
+		const handleScroll = () => {
+			window.scrollY > 300 ? setShowBtn(true) : setShowBtn(false);
+		};
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
 		<main className='px-10 lg:px-24 pb-20'>
 			<Hero dogList={dogList} />
 			<DogListForm dogList={dogList} setImages={setImages} />
 			<ImageGallery images={images} />
-			<a href='#top'>BACK TO TOP</a>
+			{showBtn && (
+				<a
+					href='#top'
+					className='fixed bottom-10 right-5 bg-blue-800 text-white p-2 rounded-lg font-bold'
+				>
+					BACK TO TOP
+				</a>
+			)}
 		</main>
 	);
 }
