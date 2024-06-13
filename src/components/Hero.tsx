a828b4e7d0179afc0ab6695ebb12fff0b390dd5e
Ariela Israel <arielaisrael12@gmail.com>
2024-06-12 20:54:01 -0400

    +added conditionals; modified widths

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,10 @@ const Hero = ({ dogList }: { dogList: string[] }) => {
 
 	// FORMATTING BREED VALUES
 	const formatBreed = (breed: string) => {
-		const breedSplit = breed.split(' ');
+		let breedSplit: string[] = [];
+		if (breed) {
+			breedSplit = breed.split(' ');
+		}
 		return breedSplit.join('-');
 	};
 
@@ -44,26 +47,30 @@ const Hero = ({ dogList }: { dogList: string[] }) => {
 		<header className='flex flex-col items-center mb-12 pt-20' id='top'>
 			<h1 className='z-20 mb-10 text-center'>
 				<div className='text-4xl lg:text-6xl -mb-2'>Welcome to</div>
-				<div className='text-8xl lg:text-9xl font-black uppercase -rotate-6 -mb-20 drop-shadow-xl bg-gradient-to-b from-transparent via-slate-800 to-transparent'>
+				<div className='text-8xl lg:text-9xl font-black uppercase -rotate-6 -mb-20 drop-shadow-2xl'>
 					Doggo
 				</div>
 			</h1>
 			<div className='z-10 flex flex-col items-center'>
 				<button type='button' onClick={getRandomImage}>
-					<img src={image} alt={image} className='w-80 lg:w-96 mb-2 rounded-lg' />
+					<img
+						src={image}
+						alt={image}
+						className='w-80 lg:w-[35rem] mb-2 rounded-lg'
+					/>
 				</button>
 				<p className='text-center animate-bounce'>
 					☝️Click the doggo to generate a random pic☝️
 				</p>
 				<div className='mt-10 bg-gradient-to-r from-transparent via-blue-800/10 to-transparent w-screen flex flex-col items-center'>
-					<div className='w-96 h-1 bg-gradient-to-r from-transparent via-blue-300 to-transparent'></div>
-					<p className='text-2xl w-96 text-center my-10'>
+					<div className='w-screen h-1 bg-gradient-to-r from-transparent via-blue-300 to-transparent'></div>
+					<p className='text-2xl w-96 lg:w-auto text-center my-10'>
 						Doggo is a dog image gallery with open source photos from{' '}
 						<a href='https://dog.ceo/dog-api/' target='_blank'>
 							DogAPI
 						</a>
 					</p>
-					<div className='w-96 h-1 bg-gradient-to-r from-transparent via-blue-300 to-transparent'></div>
+					<div className='w-screen h-1 bg-gradient-to-r from-transparent via-blue-300 to-transparent'></div>
 				</div>
 			</div>
 		</header>
